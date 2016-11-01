329b8de412a582ebdf67808b89d72a442eead212
Boopathi Rajaa <me@boopathi.in>
2016-11-02 00:44:51 +0100

    Remove somethings from prefetch and sw

--- sw.js
+++ sw.js
@@ -5,14 +5,9 @@ toolbox.cache.name = "babili-" + 6;
 toolbox.precache([
   "static/script.js",
   "static/worker.js",
-  "build/babel-core.js",
-  "build/babel-preset-babili.js",
   "static/styles.css",
   "./",
-  "//cdn.jsdelivr.net/localforage/1.4.2/localforage.min.js",
-  "//cdn.jsdelivr.net/ace/1.2.3/min/ace.js",
-  "static/ic_delete_forever_white_24px.svg",
-  "//fonts.googleapis.com/css?family=Roboto:300,400"
+  "//cdn.jsdelivr.net/ace/1.2.3/min/ace.js"
 ]);
 
 toolbox.router.get("/", toolbox.fastest);
